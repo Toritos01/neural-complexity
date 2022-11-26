bccff42cfc20a6dd492e01afdf37675f1878e192
Brandon Diaz <bld68@cornell.edu>
2022-11-26 00:43:28 -0500

    Added some simple functions for evaluation

--- zarpies/utils.py
+++ zarpies/utils.py
@@ -1,35 +1,46 @@
 from transformers import (
-    AutoModelForCausalLM, AutoModelForMaskedLM,
+    AutoModelForCausalLM,
+    AutoModelForMaskedLM,
     AutoModelForSeq2SeqLM,
-    AutoTokenizer, TrainingArguments, Trainer
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer,
 )
 from datasets import load_dataset
 import os
 import torch
-
-"""
-# Finetunes a huggingface transformer LM with given input data
-# [lm_path_or_name] = Filepath to a model to be finetuned (optional).
-#           A huggingface LM name can be used too, the use_original parameter
-#           decides which to use.
-# [data_path] = filepath to a text file containing sentences separated by newlines
-# [output_path] = path to output the model to
-# [batch_size] = Batching size for training (default: 2)
-# [use_original] = When this argument is true, the model_path is ignored, instead
-#           using an unmodified version of the original huggingface model from 
-#           original_LM_name. (default: false)
-# [maksed] = Should be true when using a masked LM, false when using incremental (default: false)
-
-Usage examples:
-Fine tuning an incremental LM that was saved as a file, and previously finetuned
-finetune("./models/mymodel", "data/my_data.txt", "output_path=./models/mymodelV2", use_original=False, masked=False )
-
-Fine tuning a masked language model (BERT), using the original model from huggingface
-finetune("bert-base-uncased", "data/my_data.txt", "./models/mymodelV2", use_original=True, masked=True )
-"""
+from minicons import scorer
+from minicons import cwe
 
 
-def finetune(lm_path_or_name, data_path, output_path, batch_size=2, use_original=False, masked=False):
+def finetune(
+    lm_path_or_name,
+    data_path,
+    output_path,
+    batch_size=2,
+    use_original=False,
+    masked=False,
+):
+    """
+    # Finetunes a huggingface transformer LM with given input data
+    # [lm_path_or_name] = Filepath to a model to be finetuned (optional).
+    #           A huggingface LM name can be used too, the use_original parameter
+    #           decides which to use.
+    # [data_path] = filepath to a text file containing sentences separated by newlines
+    # [output_path] = path to output the model to
+    # [batch_size] = Batching size for training (default: 2)
+    # [use_original] = When this argument is true, the model_path is ignored, instead
+    #           using an unmodified version of the original huggingface model from 
+    #           original_LM_name. (default: false)
+    # [maksed] = Should be true when using a masked LM, false when using incremental (default: false)
+
+    Usage examples:
+    Fine tuning an incremental LM that was saved as a file, and previously finetuned
+    finetune("./models/mymodel", "data/my_data.txt", "output_path=./models/mymodelV2", use_original=False, masked=False )
+
+    Fine tuning a masked language model (BERT), using the original model from huggingface
+    finetune("bert-base-uncased", "data/my_data.txt", "./models/mymodelV2", use_original=True, masked=True )
+    """
     tokenizer = None
     # If we are not using a previously finetuned model, we need to create/save a new tokenizer for it
     if use_original:
@@ -43,7 +54,7 @@ def finetune(lm_path_or_name, data_path, output_path, batch_size=2, use_original
     def tokenize_function(examples):
         return tokenizer(examples["text"], padding="max_length", truncation=True)
 
-    dataset = load_dataset('text', data_files=data_path)
+    dataset = load_dataset("text", data_files=data_path)
 
     tokenized_datasets = dataset.map(tokenize_function, batched=True)
 
@@ -58,41 +69,68 @@ def finetune(lm_path_or_name, data_path, output_path, batch_size=2, use_original
 
     # Using a fake output path in the training args, because saving via training
     # args didn't work when I tried, so instead I manually save after the training
-    fake_output_path = os.path.join(os.path.dirname(
-        os.path.realpath(__file__)), "models", ".ignore")
+    fake_output_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "models", ".ignore"
+    )
     training_args = TrainingArguments(
-        output_dir=fake_output_path, per_device_train_batch_size=batch_size)
+        output_dir=fake_output_path, per_device_train_batch_size=batch_size
+    )
 
     print(tokenized_datasets)
     # Add in a labels feature to the dataset so that it will train
     tokenized_datasets["train"] = tokenized_datasets["train"].add_column(
-        "labels", tokenized_datasets["train"]["input_ids"])
+        "labels", tokenized_datasets["train"]["input_ids"]
+    )
 
     trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=tokenized_datasets["train"]
+        model=model, args=training_args, train_dataset=tokenized_datasets["train"]
     )
 
     trainer.train()
     model.save_pretrained(output_path)
 
-# TODO: Create a function that takes a model (from a filepath, or from huggingface)
-# and a data array, and returns surprisal for each array element (sentence).
-# This function can use the .sequence_score function from minicons scorers
+
+def create_scorer(lm_path_or_name, masked):
+    """
+    Creates a minicon scorer from a filepath or huggingface model name.
+    Second argument should be set to true for masked models and false for
+    incremental models.
+    """
+    if masked:
+        return scorer.MaskedLMScorer(lm_path_or_name, "cpu")
+    else:
+        return scorer.IncrementalLMScorer(lm_path_or_name, "cpu")
 
 
-def evaluate_surp(lm_path_or_name, data_arr, masked):
-    return None
-
-# TODO: Create a function to evaluate the surprisal of various queries given
-# their prefixes.
-# This can use the .partial_score function from minicons scorers
-# This is useful for something like: prefix="Zarpies are" and query="evil."
+def create_cwe(lm_path_or_name):
+    """
+    Creates a minicons context aware embedding from a model name or path.
+    """
+    return cwe.CWE(lm_path_or_name)
 
 
-def evaluate_surp_conditional(lm_path_or_name, prefixes, queries, masked):
-    return None
+def evaluate_surp(lm_scorer, data_arr, reduction=lambda x: -x.sum(0).item()):
+    """
+    This function takes a minicons scorer and uses it to evaluate the surprisal
+    of the sentences in data_arr.
+    The optional parameter, [reduction], allows you to calculate something other than
+    surprisal (see minicons specs).
+    """
+    return lm_scorer.sequence_score(data_arr, reduction)
+
+
+def evaluate_surp_conditional(lm_scorer, prefixes, queries, reduction=lambda x: -x.sum(0).item()):
+    """
+    This function takes a minicons scorer and uses it to evaluate the surprisal
+    of the sentences in data_arr.
+    The optional parameter, [reduction], allows you to calculate something other than
+    surprisal (see minicons specs).
+
+    Example usage:
+    evaluate_surp_conditional("distilgpt2", ["Zarpies are", "All zarpies are"], ["evil.", "evil."])
+    """
+    return lm_scorer.partial_score(prefixes, queries, reduction)
+
 
 # TODO: Create a function that uses cosine similarity to compare the word
 # embeddings for [item1] and [item2].
@@ -101,5 +139,16 @@ def evaluate_surp_conditional(lm_path_or_name, prefixes, queries, masked):
 # and pytorch's torch.nn.CosineSimilarity function.
 
 
-def evaluate_representation_similarity(lm_path_or_name, item1, item2):
-    return None
+def evaluate_rep_sim(cwe, item1, item2):
+    """
+    This function returns the cosine similarity between the representation of
+    [item1] vs that of [item2] using the context aware embedding [cwe].
+    The formatting for item1 and item2 should be a tuple of two elements like:
+    (context, word_to_get_representation_for).
+
+    Example usage:
+    evaluate_rep_sim(cwe, ("Zarpies go to the store.", "Zarpies"), ("Carpenters go to the store.", "Carpenters"))
+    """
+    reps = cwe.extract_representation([item1, item2], layer=12)
+    cos = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
+    return cos(reps[0], reps[1])
