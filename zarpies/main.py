2b954a3f871becc5b02e7f8de216e1891ec88a76
Brandon Diaz <bld68@cornell.edu>
2022-11-26 01:49:22 -0500

    Got rid of some unused code in main

--- zarpies/main.py
+++ zarpies/main.py
@@ -5,25 +5,6 @@ import os
 import switch_cache  # <----- Comment out
 from utils import finetune
 
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-# model_path = os.path.join(dir_path, 'models', 'distilgpt2_adapted_ZT1')
-# data_path = os.path.join(dir_path, 'data', 'junkData.txt')
-# out_path = os.path.join(dir_path, 'models', 'distilAdaptedJunk')
-# finetune("distilgpt2", data_path, out_path, use_original=True, masked=False)
-
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-# # model_path = os.path.join(dir_path, 'models', 'distilgpt2_adapted_ZT1')
-# data_path = os.path.join(dir_path, 'data', 'zarpiesT1.txt')
-# out_path = os.path.join(dir_path, 'models', 'distilAdaptedJunk')
-# finetune(out_path, data_path, out_path, use_original=False, masked=False)
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-# model_path = os.path.join(dir_path, 'models', 'distilgpt2_adapted_ZT1')
-data_path = os.path.join(dir_path, 'data', 'zarpiesT1.txt')
-out_path = os.path.join(dir_path, 'models', 'bert_adapted_zarp')
-finetune("bert-base-uncased", data_path,
-         out_path, use_original=True, masked=True)
-
 # >>>>>>>>>>>>>>>>>> IMPORTANT <<<<<<<<<<<<<<<<<<
 # Set this variable to false after you run this code once, having it set to true
 # will redo all the finetune training
